d447b23d745fba23fb2825613955a15e78a94561
Josh Glynn <jglynn606@gmail.com>
2022-02-21 17:41:59 +0000

    c# number guessing game

---
+++ NumberGuessingGame/Classes/NumberGuesser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame.Classes
+{
+    public class NumberGuesser
+    {
+        private int winningNumber;
+
+        public int TotalGuesses { get; private set;  }
+
+        public enum Result
+        {
+            Win,
+            Higher,
+            Lower,
+            Lose
+        }
+
+
+        public string WelcomeText { get; } = "Hello, type your name to start";
+
+        public string PlayerName { get; set; }
+
+        public NumberGuesser()
+        {
+            // sets winning number
+            Random rand = new Random();
+            winningNumber = rand.Next(1, 11);
+
+            // sets no of tries
+            TotalGuesses = 3;
+        }
+
+        public Result MakeGuess(int guess)
+        {
+            TotalGuesses--;
+
+            if (guess == winningNumber)
+                return Result.Win;
+
+            if (guess > winningNumber && TotalGuesses > 0)
+                return Result.Higher;
+
+            if (guess < winningNumber && TotalGuesses > 0)
+                return Result.Lower;
+
+            return Result.Lose;
+        }
+
+    }
+}
---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,59 @@
+using NumberGuessingGame.Classes;
+using System;
+using static NumberGuessingGame.Classes.NumberGuesser;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var guesser = new NumberGuesser();
+
+            Console.WriteLine(guesser.WelcomeText);
+
+            guesser.PlayerName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + guesser.PlayerName + " lets play");
+
+            Console.WriteLine("You have " + guesser.TotalGuesses + " guesses left, choose your first number (between 1 and 10)");
+
+            do
+            {
+
+                // need to alert user if string not a number
+                var result = guesser.MakeGuess(int.Parse(Console.ReadLine()));
+
+                if (result == Result.Higher)
+                {
+                    Console.WriteLine("Your number is higher than the answer. You have " + guesser.TotalGuesses + " guesses left");
+                    continue;
+                }
+
+                if (result == Result.Lower)
+                {
+                    Console.WriteLine("Your number is lower than the answer. You have " + guesser.TotalGuesses + " guesses left");
+                    continue;
+                }
+
+                if (result == Result.Win)
+                {
+                    Console.WriteLine("You have won. congratulations");
+                    break;
+                }
+
+
+                if (result == Result.Lose)
+                {
+                    Console.WriteLine("You have lost. :(");
+                    break;
+                }
+
+
+            } while (true);
+
+            Console.ReadKey();
+
+        }
+    }
+}
